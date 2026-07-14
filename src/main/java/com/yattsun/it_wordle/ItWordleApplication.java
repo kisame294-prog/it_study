62b87305d00488a22a9de6689828a41e1b1d5ae4
やっつん。 <kisame294@gmail.com>
2026-07-14 11:43:45 +0900

    IT用語限定のWordle

---
+++ src/main/java/com/yattsun/it_wordle/ItWordleApplication.java
@@ -0,0 +1,13 @@
+package com.yattsun.it_wordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItWordleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItWordleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yattsun/it_wordle/ItWordleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yattsun.it_wordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItWordleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
